2eee6e5e7769b2e176f26e2d16b613fe655c8067
Oceanoazul <ngvelasco@gmail.com>
2024-12-28 17:09:48 +0100

    actualizacion del curso

---
+++ 04_hello_world_translation/models/pets.py
@@ -0,0 +1,12 @@
+from odoo import models, fields, api
+
+class pets(models.Model):
+    _name = 'ej.pets'
+    
+    name = fields.Char(string='name', required=True)
+    age = fields.Integer(string='age')
+    color = fields.Char(string='color')
+    type = fields.Selection([('small', 'Small'),
+                            ('medium', 'Medium'), 
+                            ('bige', 'Barge')], string='Type', default='small', required=True)
+    
